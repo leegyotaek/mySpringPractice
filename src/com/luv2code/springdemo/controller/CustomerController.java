e57762c7ed117ba18f068939fe8454e28cefb0be
leegyotaek <leegyotaek@gmail.com>
2020-12-15 21:47:22 +0900

    create CustomeDao, CustomerDaoImpl create CustomerController creat jsp Page : list-customer.jsp

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,17 +1,31 @@
 package com.luv2code.springdemo.controller;
 
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@Autowired
+	private CustomerDAO customerDAO;
+
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
-		
+
+		List<Customer> customers = customerDAO.getCustomers();
+
+		theModel.addAttribute("customers", customers);
+
 		return "list-customers";
+
 	}
 	
 }
